4d22f92e1168d4aa184430f7f5045273a7023892
0xalphaalert <dkrout006@gmail.com>
2026-07-25 19:34:58 +0530

    feat: implement mobile article modal, markdown rendering, and interactive header actions

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.d7ehpmmmil8"
+    "revision": "0.jn7p4mftucs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
